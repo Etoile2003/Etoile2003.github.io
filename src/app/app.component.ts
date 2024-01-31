ce61d8de1be268da2959c8a45b73832264cc9da1
Bastien <bastien.pelletier01@gmail.com>
2024-01-31 19:18:43 +0100

    adding fouter and nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet , NavBarComponent , FooterComponent , PresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
